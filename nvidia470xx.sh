a4bc9f1157303ef14184314582b0d6650157c51f
restored <86575196+NoNFake@users.noreply.github.com>
2022-07-16 19:52:32 +0000

    Add files via upload

---
+++ nvidia470xx.sh
@@ -0,0 +1,13 @@
+echo instaling git then yay
+sleep 1
+sudo pacman -Sy git
+sudo git clone https://aur.archlinux.org/yay-git.git
+echo write your username
+read name
+sudo chown -R $name ./yay-git
+cd yay-git
+makepkg -si
+sudo yay -Syu
+yay -Syu --devel --timeupdate
+yay -S linux-headers nvidia-470xx-dkms nvidia-470xx-settings nvidia-470xx-utils
+sudo pacman -Suy
