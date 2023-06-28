46dd44bd088585f8336d374e2d314ab08d8b8e89
Restored [.-.] <86575196+NoNFake@users.noreply.github.com>
2023-06-28 12:53:04 +0300

    Update nvidia470xx.sh

--- nvidia470xx.sh
+++ nvidia470xx.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-# Install go
-sudo pacman -Sy go
-
-# Create a new user to build and install packages
-sudo useradd -m pkgbuilder
-
-# Clone yay-git repository and build package
-sudo -u pkgbuilder bash -c 'cd ~ && git clone https://aur.archlinux.org/yay-git.git && cd yay-git && makepkg -si'
-
-# Remove pkgbuilder user
-sudo userdel -r pkgbuilder
-
-# Update system and install Nvidia 470 driver and dependencies
-yay -Syu --noconfirm
-yay -S --noconfirm linux-headers nvidia-470xx-dkms nvidia-470xx-settings nvidia-470xx-utils
-
-# Update system again
-sudo pacman -Suy --noconfirm
+sudo pacman -S linux-headers dkms
+yay -S nvidia-470xx-dkms nvidia-470xx-settings
 
 echo "Done."
