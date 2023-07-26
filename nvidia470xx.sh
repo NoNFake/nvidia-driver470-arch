d94222bfea1221bc86eff2632d25b01fba37dc39
Restored [.-.] <86575196+NoNFake@users.noreply.github.com>
2023-07-26 22:04:37 +0300

    Update nvidia470xx.sh
    For new NVIDIA

--- nvidia470xx.sh
+++ nvidia470xx.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
-sudo pacman -S linux-headers dkms
-yay -S nvidia-470xx-dkms nvidia-470xx-settings
-
-echo "Done."
+echo instaling git then yay
+sleep 1
+sudo pacman -Sy git
+sudo git clone https://aur.archlinux.org/yay-git.git
+echo write your username
+read name
+sudo chown -R $name ./yay-git
+cd yay-git
+makepkg -si
+sudo yay -Syu
+yay -Syu --devel --timeupdate
+yay -S linux-headers nvidia-dkms nvidia-settings nvidia-utils
+sudo pacman -Suy
